ae6e742d695d04419d388b354e103c7adfdddd04
Graeme Leighfield <graeme@duxter.com>
2013-05-13 11:24:22 +0100

    Inclusion of bootstrap files into includes

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -1,5 +1,7 @@
 
 <!-- FOOTER START //-->
+		<script src="assets/js/jquery-1.8.2.min.js"></script>
+		<script src="assets/js/bootstrap.min.js"></script>
 	</body>
 </html>
 <!-- FOOTER END //-->
--- admin/includes/header.php
+++ admin/includes/header.php
@@ -1,7 +1,14 @@
+<?php
+	//Include global config file
+	require_once('config.php');
+?>
 <!-- HEADER START //-->
 <!DOCTYPE html>
 <html>
 	<head>
+		<title></title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href="assets/css/bootstrap.min.css" rel="stylesheet" media="screen">
 	
 	</head>
 	<body>
