d35b62ba6a2e9aa3c89ceb33a331ecb3954e3fb8
Graeme Leighfield <graeme@duxter.com>
2013-05-20 15:15:00 +0100

    Inclusion of CKEditor

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -5,6 +5,7 @@
 		</div>
 		<script src="assets/js/jquery-1.8.2.min.js"></script>
 		<script src="assets/js/bootstrap.min.js"></script>
+		<script src="assets/ckeditor/ckeditor.js"></script>
 		<script src="script.js"></script>
 	</body>
 </html>
