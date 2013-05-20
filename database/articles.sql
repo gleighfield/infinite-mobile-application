6685c2d73f0608022d0ee3e2b20449b00c4743b1
Graeme Leighfield <graeme@duxter.com>
2013-05-20 15:31:01 +0100

    Add userid and alert to articles table

--- database/articles.sql
+++ database/articles.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: May 20, 2013 at 03:13 PM
+-- Generation Time: May 20, 2013 at 03:30 PM
 -- Server version: 5.1.68-cll
 -- PHP Version: 5.3.17
 
@@ -30,7 +30,9 @@ CREATE TABLE IF NOT EXISTS `articles` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `channel` int(2) NOT NULL,
   `title` varchar(100) NOT NULL,
+  `alert` int(1) NOT NULL,
   `content` longtext NOT NULL,
+  `user_id` int(4) NOT NULL,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
