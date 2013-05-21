263a46ebc9b49e885b23c070a3c560119ac213ff
Graeme Leighfield <graeme@duxter.com>
2013-05-21 18:24:36 +0100

    Inclusion of channel_name for settings page

--- admin/ajax/device/user_login.php
+++ admin/ajax/device/user_login.php
@@ -8,8 +8,10 @@
 		'user_email'		=> $_POST['user_email'],
 		'time_stamp'		=> date('Y-m-d H:i:s'),
 	);
+
+	$data['user_email'] = "graeme@gelstudios.co.uk";
 	
-	//Add channel to DB
+	//Return user information
 	$sql = "SELECT id, firstname, lastname, channel FROM users WHERE email = :email";
 	$userQuery = $db->prepare($sql);
 	$userQuery->execute(array(
@@ -22,13 +24,25 @@
 		header('HTTP/1.1 403 ' . $s['login_error']);
 		return false;
 	}
+
+	//Return user information
+	$sql = "SELECT name FROM channels WHERE id = :channelId";
+	$channelQuery = $db->prepare($sql);
+	$channelQuery->execute(array(
+		':channelId'	=> $user['channel']
+	));
+
+	$channel = $channelQuery->fetch(PDO::FETCH_ASSOC);
+
+	//Add channel name
+	$user['channel_name'] = $channel['name'];
 	
 	//Add log entry
 	$sql = "INSERT INTO user_log (user_id, comment, timestamp) VALUES (:user_id, :comment, :timestamp)";
 	$addLogEntry = $db->prepare($sql);
 	$addLogEntry->execute(array(
-		':user_id'		=> $user['id'],
-		':comment'		=> $user['firstname'] . ' '. $user['lastname'] . '(' . $data['user_email'] . ') linked!',
+		':user_id'	=> $user['id'],
+		':comment'	=> $user['firstname'] . ' '. $user['lastname'] . '(' . $data['user_email'] . ') linked!',
 		':timestamp'	=> $data['time_stamp']
 	));
 	
