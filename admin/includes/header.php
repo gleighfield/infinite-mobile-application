6dfecd65907a51758475f16f8c2c065b8f259872
Graeme Leighfield <graeme@gelstudios.co.uk>
2013-06-11 14:26:03 +0100

    Admin - Article encoding issues resolved

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -7,6 +7,7 @@
 <html>
 	<head>
 		<title></title>
+        <META HTTP-EQUIV="CONTENT-TYPE" CONTENT="text/html; charset=UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link href="assets/css/bootstrap.min.css" rel="stylesheet" media="screen">
 		<link href="assets/css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
